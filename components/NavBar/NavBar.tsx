e55d5ed777672b0fcdb45df7767bcd1d8302ebfd
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2025-09-04 21:43:58 +0100

    base

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+export default function NavBar({ sample }: { sample?: boolean }) {
+  return sample ? (
+    <nav className="w-full flex items-center justify-between py-4 px-8 bg-primary text-white">
+      <div className="text-lg font-bold">MyApp</div>
+      <div className="space-x-4">
+        {/* <Link href="#" className="hover:underline"> */}
+        <Link href="#" className="hover:underline">
+          Home
+        </Link>
+        <Link href="signup" className="hover:underline">
+          Sign Up
+        </Link>
+        <Link href="login" className="hover:underline">
+          Login
+        </Link>
+        <Link href="#" className="hover:underline">
+          About
+        </Link>
+      </div>
+    </nav>
+  ) : (
+    <></>
+  );
+}
---
+++ components/sign-up/Signup.tsx
@@ -0,0 +1,95 @@
+'use client';
+import Image from 'next/image';
+import z from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: 'Username must be at least 2 characters' })
+    .max(20, { message: 'Username cannot be more than 20 characters' }),
+  email: z.email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(4, { message: 'Password must be at least 4 characters' })
+    .max(20, { message: 'Password cannot be more than 20 characters' }),
+});
+
+const onSubmit = () => {};
+
+export function Signup() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        <FormField
+          name="username"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              {/* <FormDescription>This is your public display name.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="email"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="" {...field} />
+              </FormControl>
+              {/* <FormDescription>This is your email address.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="password"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="" {...field} />
+              </FormControl>
+              {/* <FormDescription>This is your password.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* <div className="flex justify-center"> */}
+        <Button type="submit" className="w-full">
+          Submit
+        </Button>
+        {/* </div> */}
+      </form>
+    </Form>
+  );
+}
