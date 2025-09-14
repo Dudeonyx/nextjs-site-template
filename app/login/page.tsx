b3c07f8f7b319b1a531ea0ec6f5ad7eeb7e0779e
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2025-09-14 19:21:36 +0100

    pre changes to vi emp

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,82 @@
+'use client';
+
+import z from 'zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+
+const loginFormSchema = z.object({
+  email: z.email('Invalid email address'),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters' }),
+});
+type LoginFormSchema = z.infer<typeof loginFormSchema>;
+
+async function onSubmit(data: LoginFormSchema) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
 export default function Login() {
-  return <div>Login Page</div>;
+  const form = useForm<LoginFormSchema>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Enter your login details to access your profile</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                name="email"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Your email address" type="email" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="password"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Your password" type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
+                {form.formState.isSubmitting ? 'Logging in...' : 'Log In'}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
