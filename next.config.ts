c5a8f9b64c29e44fc3b9acd9dd0d99633e80ccbc
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2025-09-19 10:08:59 +0100

    fix: removed output: export and image unoptimised true from next config, fixing hosting issues

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
 
-  output: 'export',
+  // output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
-  images: { unoptimized: true },
+  // images: { unoptimized: true },
 };
 export default nextConfig;
